f1bce80f44803eac6041c67eed2a21c4440f1966
Michael Umeokoli <michaeljunior794@gmail.com>
2022-03-12 15:24:45 +0100

    'cors-header-fix'

--- Test/settings.py
+++ Test/settings.py
@@ -136,9 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     'http://localhost:3000'
+    'https://mike-s-react-test.netlify.app'
 ]
